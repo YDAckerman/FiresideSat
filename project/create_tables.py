e174f21f1dab20938e500b08ff45c442bfa0da52
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2022-08-23 10:41:22 -0700

    create_tables v1

--- project/create_tables.py
+++ project/create_tables.py
@@ -3,19 +3,29 @@ import configparser
 from sql_queries import drop_table_queries
 from sql_queries import create_table_queries
 
-config = configparser.ConfigParser()
-config.read_file(open('inReach.cfg'))
 
-con = sqlite3.connect(config.get('SQLITE', 'DBNAME'))
-cur = con.cursor()
+def main():
 
-print("______DROPPING TABLES______")
-for qry in drop_table_queries:
-    cur.execute(qry)
+    config = configparser.ConfigParser()
+    config.read_file(open('inReach.cfg'))
 
-print("______CREATING TABLES______")
-for qry in create_table_queries:
-    cur.execute(qry)
+    con = sqlite3.connect(config.get('SQLITE', 'DBNAME'))
+    cur = con.cursor()
 
-con.commit()
-con.close()
+    check = input("Really Drop Tables? Y/n")
+    if check == "Y":
+
+        print("______DROPPING TABLES______")
+        for qry in drop_table_queries:
+            cur.execute(qry)
+
+        print("______CREATING TABLES______")
+        for qry in create_table_queries:
+            cur.execute(qry)
+
+    con.commit()
+    con.close()
+
+
+if __name__ == "__main__":
+    main()
