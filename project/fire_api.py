64b7d8d83f48d1ba739d137e7d89e5e98ac44c5a
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2022-09-05 16:11:51 -0700

    added logging

--- project/fire_api.py
+++ project/fire_api.py
@@ -11,6 +11,7 @@
 import requests
 import json
 from perimeter import Perimeter
+from log import log
 
 
 class FireAPI:
@@ -20,30 +21,39 @@ class FireAPI:
         "FeatureServer/0/query?f=json&where=" + \
         "(irwin_POOState%20IN%20(%27US-CA%27))&outFields=*"
 
-    def __init__(self, loc):
+    def __init__(self):
 
         perimeter_json = json.loads(requests.get(self.api_call).text)
-        self.perimeters = self.build_perimeters(perimeter_json)
 
-    def build_perimeters(json_response):
-
-        perimeter_dict = {
+        self.perimeters = {
             feature['attributes']['poly_IncidentName']:
             {'behavior': feature['attributes']['irwin_FireBehaviorGeneral'],
              'perimeter': Perimeter(feature['geometry']['rings'])}
-            for feature in json_response['features']
+            for feature in perimeter_json['features']
         }
 
-        return(perimeter_dict)
+        log.info(hash(str(self.perimeters)))
 
     def build_message(self, loc):
 
-        message = []
+        message = ''
         for fire in self.perimeters.keys():
             dist = self.perimeters[fire]['perimeter'].getDist(loc)
             edge = self.perimeters[fire]['perimeter'].nearest_node
             status = self.perimeters[fire]['behavior']
             if dist < 161:
-                message += f'NAME: {fire}, STATUS: {status}, EDGE: {edge} \n'
+                message += f'{fire}, {status}, {edge}, {dist} \n'
 
         return(message)
+
+
+def main():
+    api = FireAPI()
+    msg = api.build_message(loc=[-119.602469, 37.491798])
+    if msg == '':
+        msg = 'No incidents to report'
+    print(msg)
+
+
+if __name__ == '__main__':
+    main()
--- project/firesidesat.py
+++ project/firesidesat.py
@@ -1,7 +1,7 @@
 import inReach as mini
 from fire_api import FireAPI
 import configparser
-
+from log import log
 
 def main():
 
@@ -15,14 +15,18 @@ def main():
     status = mini.get_user_info(user, pw)
     if status['event'] in ['ON', 'OTHER']:
         api = FireAPI()
+        api_hash = hash(api)
         # get hash of previous message
-        msg = api.build_message(loc=[status['lon'], status['lat']])
-        if len(msg) > 0: # and hash is different from previous
-            mini.send_user_message(user, pw, device, msg)
-            # log the response
-            # https://docs.python.org/3/howto/logging.html
-        else:
-            # log that there were no new incidents to report
+        with open('last_hash.txt', 'wr') as f:
+            last_hash = f.readlines()[-1]
+            if last_hash != api_hash:
+                msg = api.build_message(loc=[status['lon'], status['lat']])
+                if len(msg) > 0: # and hash is different from previous
+                    mini.send_user_message(user, pw, device, msg)
+                    # log the response
+                    # https://docs.python.org/3/howto/logging.html
+                else:
+                    # log that there were no new incidents to report
     else:
         # log that the device was in the OFF state
 
