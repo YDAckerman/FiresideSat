6f156b6285d727ec7e9f251d95b444fffb5add04
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2023-05-10 22:05:21 -0700

    starting to test /register and add_new_user

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -1,14 +1,19 @@
 import os
-from flask import Flask, render_template
+import sys
+sys.path.insert(0, os.path.abspath("./flaskr"))
+
+from flask import Flask, render_template, request
+from helpers.db import get_conn
+from helpers.transactions import add_new_user
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    # )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -24,16 +29,27 @@ def create_app(test_config=None):
     #     pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    # @app.route('/hello')
+    # def hello():
+    #     return 'Hello, World!'
 
     @app.route('/')
     def index():
         return render_template("index.html")
 
-    @app.route('/signup')
-    def sign_up():
-        return render_template("sign_up.html")
+    @app.route('/new_user')
+    def new_user():
+        return render_template("new_user.html")
+
+    @app.route('/register', methods=['POST'])
+    def register():
+        # email = request.form.get('email')
+        # mapshare_id = request.form.get('mapshare_id')
+        # mapshare_password = request.form.get('mapshare_password')
+        # conn = get_conn()
+        # result = add_new_user(conn, email, mapshare_id, mapshare_password)
+        result = ["success!"]
+
+        Return render_template("new_user_response.html", result=result)
 
     return app
